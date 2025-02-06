3abc65351d514c39f2d952404bb1ef5a1242ff06
Vedant125 <vedant3558@gmail.com>
2025-02-06 15:32:38 +0530

    Third Commit

--- src/get_data.py
+++ src/get_data.py
@@ -26,4 +26,4 @@ if __name__=="__main__":
     args.add_argument("--config",default= "params.yml")
     parsed_args = args.parse_args()
     data = get_data(config_path = parsed_args.config)
-    # return config 
+    
